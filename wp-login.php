8c855cd89dfa8aeb6ea3b5177e742258dbea425b
Viruscomunicacao <web@viruscomunicacao.com.br>
2022-03-17 18:16:01 -0300

    Changes bd livros

--- wp-login.php
+++ wp-login.php
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-BR" id="html">
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Acessar &lsaquo; BD Livros &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="dns-prefetch" href="//s.w.org">
-<link rel="stylesheet" id="dashicons-css" href="./wp-includes/css/dashicons.min.css?ver=5.9" media="all">
-<link rel="stylesheet" id="buttons-css" href="./wp-includes/css/buttons.min.css?ver=5.9" media="all">
-<link rel="stylesheet" id="forms-css" href="./wp-admin/css/forms.min.css?ver=5.9" media="all">
-<link rel="stylesheet" id="l10n-css" href="./wp-admin/css/l10n.min.css?ver=5.9" media="all">
-<link rel="stylesheet" id="login-css" href="./wp-admin/css/login.min.css?ver=5.9" media="all">
+<link rel="stylesheet" id="dashicons-css" href="./wp-includes/css/dashicons.min.css?ver=5.9.2" media="all">
+<link rel="stylesheet" id="buttons-css" href="./wp-includes/css/buttons.min.css?ver=5.9.2" media="all">
+<link rel="stylesheet" id="forms-css" href="./wp-admin/css/forms.min.css?ver=5.9.2" media="all">
+<link rel="stylesheet" id="l10n-css" href="./wp-admin/css/l10n.min.css?ver=5.9.2" media="all">
+<link rel="stylesheet" id="login-css" href="./wp-admin/css/login.min.css?ver=5.9.2" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
-		</head>
+	<link rel="icon" href="./wp-content/uploads/2022/03/BD_Livros-Logo-2-150x150.png" sizes="32x32">
+<link rel="icon" href="./wp-content/uploads/2022/03/BD_Livros-Logo-2.png" sizes="192x192">
+<link rel="apple-touch-icon" href="./wp-content/uploads/2022/03/BD_Livros-Logo-2.png">
+<meta name="msapplication-TileImage" content="./wp-content/uploads/2022/03/BD_Livros-Logo-2.png">
+	</head>
 	<body class="login no-js login-action-login wp-core-ui  locale-pt-br">
 	<script type="text/javascript">
 		document.body.className = document.body.className.replace('no-js','js');
@@ -44,7 +48,7 @@
 		</form>
 
 					<p id="nav">
-								<a href="./?action=lostpassword">Perdeu a senha?</a>
+								<a href="./minha-conta/lost-password/index.html">Perdeu a senha?</a>
 			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
@@ -93,14 +97,14 @@ var pwsL10n = {"unknown":"Nível de segurança da senha desconhecido","short":"M
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2022-01-23 23:05:34+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n > 1;","lang":"pt_BR"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s está obsoleta desde a versão %2$s. Use %3$s como alternativa. Considere escrever um código mais inclusivo."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
 </script>
-<script src="./wp-admin/js/password-strength-meter.min.js?ver=5.9" id="password-strength-meter-js"></script>
+<script src="./wp-admin/js/password-strength-meter.min.js?ver=5.9.2" id="password-strength-meter-js"></script>
 <script src="./wp-includes/js/underscore.min.js?ver=1.13.1" id="underscore-js"></script>
 <script id="wp-util-js-extra">
 var _wpUtilSettings = {"ajax":{"url":"\/bdlivros\/wp-admin\/admin-ajax.php"}};
 </script>
-<script src="./wp-includes/js/wp-util.min.js?ver=5.9" id="wp-util-js"></script>
+<script src="./wp-includes/js/wp-util.min.js?ver=5.9.2" id="wp-util-js"></script>
 <script id="user-profile-js-extra">
-var userProfileL10n = {"user_id":"0","nonce":"731e9b16f4"};
+var userProfileL10n = {"user_id":"0","nonce":"b7988684cf"};
 </script>
 <script id="user-profile-js-translations">
 ( function( domain, translations ) {
@@ -109,7 +113,7 @@ var userProfileL10n = {"user_id":"0","nonce":"731e9b16f4"};
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2022-01-23 23:05:34+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n > 1;","lang":"pt_BR"},"Your new password has not been saved.":["Sua nova senha não foi salva."],"Hide":["Esconder"],"Show":["Mostrar"],"Confirm use of weak password":["Confirmar o uso de uma senha fraca"],"Hide password":["Ocultar senha"],"Show password":["Mostrar senha"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
 </script>
-<script src="./wp-admin/js/user-profile.min.js?ver=5.9" id="user-profile-js"></script>
+<script src="./wp-admin/js/user-profile.min.js?ver=5.9.2" id="user-profile-js"></script>
 			<script>
 			/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
 			</script>
